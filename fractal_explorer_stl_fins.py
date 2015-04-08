6293fd218a576f313ab20512b523fd68b3d15df4
Ryan G. Coleman <ryan.g.coleman@gmail.com>
2015-04-07 19:54:23 -0600

    non-working adding fin contours

--- fractal_explorer_stl_fins.py
+++ fractal_explorer_stl_fins.py
@@ -4,13 +4,24 @@ import sys
 import math
 import geometry
 
-#prisms = [[(0,0,0), (40,0,0), (5,20,0), (25,20,0),
-#    (0,0,0.7), (40,0,0.7), (5,20,0.7), (25,20,0.7)]]
+def add_tops(prisms,
+             thicknessMap={0:1.4, 1:1.3, 2:1.2, 3:1.1, 4:1.0, 5:0.9, 6:0.8, 7:0.7},
+             minThickness=0.6):
+#prisms = [[[5.,0.,0.], [50.,0.,0.], [8.5,20.,0.], [33.,20.,0.]]]
+  for count, one_list in enumaret(prisms):
+    top1 = [one_list[1][0], one_list[2][1], one_list[0][3]]
+    top2 = [one_list[3][0], one_list[2][1], one_list[0][3]]
+    thickness = minThickness
+    if count > 0:
+      layer_count = int(math.log(count, 2))
+    else:
+      layer_count = 0
+    if layer_count in thicknessMap.keys():
+      thickness = thicknessMap[layer_count]
+  
 
-def add_one_layer(prisms, which=-1, scale=0.54):
+def add_one_layer(prisms, which=-1, vscale=0.54, hscale=0.62):
   last_layer = prisms[which][:]
-  dir = [0., last_layer[2][1] - last_layer[0][1],
-         last_layer[2][2] - last_layer[0][2]]
   for count in xrange(2):
     new_layer = []
     sin_t = math.sin(math.pi/3)
@@ -19,15 +30,12 @@ def add_one_layer(prisms, which=-1, scale=0.54):
       sin_t = math.sin(-math.pi/3)
       cos_t = math.cos(-math.pi/3)
     for point_count, point in enumerate(last_layer):
-      current = [(11.0+point[0])*scale,
-                 (point[1]-last_layer[0][1])*scale,
-                 (point[2]-last_layer[0][2])*scale]
+      current = [(11.0+point[0])*vscale,
+                 (point[1]-last_layer[0][1])*hscale,
+                 (point[2]-last_layer[0][2])*hscale]
       new_layer.append([current[0],
                         current[1]*cos_t - current[2]*sin_t + last_layer[2][1],
                         current[2]*cos_t + current[1]*sin_t + last_layer[2][2]])
-#      new_layer.append([current[0],
-#                        current[1]*cos_t - current[2]*sin_t + dir[1],
-#                        current[2]*cos_t + current[1]*sin_t + dir[2]])
     prisms.append(new_layer)
 
 def print_face(face1, face2, face3):
@@ -56,8 +64,25 @@ def print_prisms(prisms):
     print_face(prism[5], prism[6], prism[7])
   print 'endsolid fractal_explorer_stl_fins'
 
+def print_prisms_more(prisms):
+  print 'solid fractal_explorer_stl_fins'
+  for prism in prisms:
+    print_face(prism[0], prism[1], prism[2])
+    print_face(prism[1], prism[2], prism[3]) #side1
+    print_face(prism[0], prism[4], prism[1])
+    print_face(prism[1], prism[4], prism[5]) #top
+    print_face(prism[2], prism[4], prism[6])
+    print_face(prism[7], prism[4], prism[6]) #back
+    print_face(prism[2], prism[3], prism[6])
+    print_face(prism[7], prism[3], prism[6]) #front
+    print_face(prism[0], prism[2], prism[4]) 
+    print_face(prism[6], prism[2], prism[4]) #bottom
+    print_face(prism[4], prism[5], prism[6])
+    print_face(prism[5], prism[6], prism[7]) #side2
+  print 'endsolid fractal_explorer_stl_fins'
+
 def thickenate(prisms,
-               thicknessMap={0:1.0, 1:0.9, 2:0.8, 3:0.7, 4:0.7, 5:0.7},
+               thicknessMap={0:1.4, 1:1.3, 2:1.2, 3:1.1, 4:1.0, 5:0.9, 6:0.8, 7:0.7},
                minThickness=0.6):
   for count, prism in enumerate(prisms):
     thickness = minThickness
@@ -82,7 +107,7 @@ def thickenate(prisms,
     prism.extend(newPoints)
 
 prisms = [[[5.,0.,0.], [50.,0.,0.], [8.5,20.,0.], [33.,20.,0.]]]
-for count in xrange(1000):
+for count in xrange(2**8):
   add_one_layer(prisms, count)
 thickenate(prisms)
 print_prisms(prisms)
